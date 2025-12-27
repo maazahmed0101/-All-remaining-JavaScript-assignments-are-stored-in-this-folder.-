3d00ea7ce84e8803d787e6d290f889d12cacede4
Maaz-Ahmed <wwwmaazahmed1@gmail.com>
2025-12-27 16:14:48 +0500

    ok

---
+++ Js-7/app.js
@@ -0,0 +1,63 @@
+// QUECTION--1;
+// let user = Number(prompt('enter some number'));
+// let first = user
+// let secound = Math.floor(user)
+// let third = Math.ceil(user)
+// let forth = Math.round(user)
+// console.log(first)
+// console.log(secound)
+// console.log(third)
+// console.log(forth)
+
+// quection--2;
+// let user = Number(prompt('enter some number'));
+// let first = user
+// let secound = Math.floor(user)
+// let third = Math.ceil(user)
+// let forth = Math.round(user)
+// console.log(first)
+// console.log(secound)
+// console.log(third)
+// console.log(forth)
+
+// QUECTIOM--3;
+// let user = Number(-4);
+// let first = Math.abs(user)
+// console.log(first)
+
+// quection--4
+let user = Number(6);
+let first = Math.random(user)
+console.log(first)
+
+// QUECTION--8;
+// let user = '';
+// if(user > 6){
+//     console.log('number is greather then 6');
+// }else if(user===6){
+//     console.log('ok')
+// }else if(user < 6){
+//     console.log('number is smaller then 6');
+// }else{
+//     console.log('not defined');
+// }
+
+// quection--7
+// let weight = prompt('enter');
+// let ok = parseFloat(weight);
+// console.log(ok)
+
+// quection--6;
+// let random = 1;
+// random = Math.random()*100
+// console.log(random)
+
+// QUECTION--5;
+// let random = '';
+// random = Math.random()*50;
+// if(random>5){
+//     console.log('head')
+// }else{
+//     console.log('tail')
+// }
+// console.log(random)
