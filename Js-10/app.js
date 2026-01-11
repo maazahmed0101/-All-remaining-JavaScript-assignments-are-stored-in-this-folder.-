c234e95ac5d8317331f951394af765cc36823dcd
Maaz-Ahmed <wwwmaazahmed1@gmail.com>
2026-01-11 09:06:40 +0500

    new

---
+++ Js-10/app.js
@@ -0,0 +1,19 @@
+// quection 1
+let num = 3
+let num2 = 3;
+
+for(let i = num ; i <= num2 ; i++){
+    let ok = num * num2
+    console.log(ok)
+}
+
+// quection-2;
+
+let year = [2026 , 2024]
+let ok = year.filter((ok)=>{
+    return ok  % 4=== 0
+})
+console.log(ok)
+
+// quection--3;
+let 
