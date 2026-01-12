62a9c10f8fecc0167c3492d544061401a11f4b1a
Maaz-Ahmed <wwwmaazahmed1@gmail.com>
2026-01-13 01:49:10 +0500

    major

--- Js-10/app.js
+++ Js-10/app.js
@@ -1,19 +1,45 @@
-// quection 1
-let num = 3
-let num2 = 3;
+// // quection 1
+// let num = 3
+// let num2 = 3;
+// let results = []
 
-for(let i = num ; i <= num2 ; i++){
-    let ok = num * num2
-    console.log(ok)
-}
+// for(let i = num ; i <= num2 ; i++){
+//     results = num * num2
+//     console.log(results)
+// }
 
-// quection-2;
+// // quection-2;
 
-let year = [2026 , 2024]
-let ok = year.filter((ok)=>{
-    return ok  % 4=== 0
-})
-console.log(ok)
+// let year = [2026 , 2024]
+// let ok = year.filter((ok)=>{
+//     return ok  % 4=== 0
+// })
+// console.log(ok)
 
-// quection--3;
-let 
+// // quection--3;
+
+// let fun1 = (a, b, c)=>{
+//    return (a + b + c) / 2
+// }
+// console.log(fun1())
+
+// let fun2 = (a , b , c)=>{
+//     let s = fun1(a , b , c)
+
+//     let x = s - a
+//     let y = s - b
+//     let z = s - c
+     
+// let equal = x * y * z
+
+//     return Math.sqrt(equal)
+// }
+// console.log(fun1(3,4,5))
+// console.log(fun2(3,4,5))
+
+// QUECTION--6
+// let num3 = [1,3,5,6]
+// let filter = num3.filter((ok)=>{
+//     return ok % 2 !== 0
+// })
+// console.log(filter)
---
+++ Js-11/app.js
@@ -0,0 +1,10 @@
+let fun = ()=>{
+    return alert('hi')
+}
+let fun2 = (ok)=>{
+    let row =  ok.parentNode.parentNode
+    return row.remove();
+}
+let count = ()=>{
+    
+}
