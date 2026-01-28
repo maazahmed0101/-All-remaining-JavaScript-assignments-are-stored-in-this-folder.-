35f9002182a5dc211f95e410ac095adfb93b3aac
Maaz-Ahmed <wwwmaazahmed1@gmail.com>
2026-01-29 03:16:41 +0500

    geo

--- Js__12/app.js
+++ Js__12/app.js
@@ -1,7 +1,7 @@
-// let fun = ()=>{
-//     let i = document.getElementById('ok')
-//     console.log(i.value);
-// }
+let fun = ()=>{
+    let i = document.getElementById('ok')
+    console.log(i.value);
+}
 
 // console.log('ok');
 
@@ -20,14 +20,46 @@ let pass = ()=>{
 
     return false
 }
-let more = ()=>{
-    let mor = document.getElementById('hi')
-    let ok = mor.innerHTML = ' Lorem ipsum dolor sit amet consectetu';
-    
-    return false;
 
-}
 
-let students()=>{
+function more(){
+   
+    document.getElementById('hi').innerHTML = ` Lorem ipsum dolor sit amet consectetu Lorem ipsum dolor sit amet consectetu, adipisicing elit, sed do eiusmod tempor incididuntLorem ipsum dolor sit amet consectetu, adipisicing elit, sed do eiusmod tempor incididunt <br> <br>
+     <a href="#" onclick="less()">see less</a>`;
     
 }
+
+function less(){
+    document.getElementById(`hi`).innerHTML = `Lorem ipsum dolor sit amet consectetu Lorem ipsum dolor sit amet consectetu,    
+    <a href = '#' onclick = 'more()' > seemore </a> ` 
+}
+// console.log(window.more)
+
+function foo(){
+    let ok =  document.getElementById('name').value
+    document.getElementById('nam').innerHTML = ok
+
+    let element = document.getElementById("studenttable")
+
+    let row = table.insertrow();
+    let cell1 = row.insertrow(0);
+    let cell2 = row.insertrow(1);
+    let cell3 = row.insertrow(2);
+     let cell4 = row.insertCell(3);
+
+    cell1.innerText = name;
+    cell2.innerText = '-';
+    cell3.innerText = '-';
+
+    let delbtn = document.createElement('button');
+    delbtn.innerText = 'delete';
+    delbtn.onclick = function(){
+        table.deleterow(row.index);
+    }
+    cell4.appendChild(delbtn);
+
+    document.getElementById('name').value = ""
+}
+// function geo(){
+//     document.getElementById('nam').innerHTML = 
+// }
